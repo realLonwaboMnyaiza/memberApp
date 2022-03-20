5ddf4d341f1da2bb6517e8697a6516bd76f159c9
Lonwabo Mnyaiza <lonwabom@kingjames.digital>
2022-03-20 12:02:06 +0200

    :package: adding member get endpoint.

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using app.DAL;
+using app.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger _log;
+        public MemberController(AppDbContext context,
+                                ILogger log)
+        {
+            _context = context;
+            _log = log;
+        }
+
+        [HttpGet("/api/v1/members")]
+        public IEnumerable<Member> GetMembers()
+        {
+            return _context.Member.ToList();
+        }
+    }
+}
