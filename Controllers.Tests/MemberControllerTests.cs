ca8f0cf10cebf10ca8a83480a52ad92be2336a65
Lonwabo Mnyaiza <lonwabom@kingjames.digital>
2022-03-21 14:02:20 +0200

    :white_check_mark: adding testing scaffold.

---
+++ Controllers.Tests/MemberControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using app.Models;
+
+namespace app.Controllers.Tests
+{
+    [TestFixture]
+    public class MemberControllerTests
+    {
+        [Test]
+        public void ShouldBeImplemented()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Member Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
